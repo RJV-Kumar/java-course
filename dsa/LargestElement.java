a8b26bacbdfef4c045a9465919012bbf4b7b3207
AutomateSphere <rajivkumar3636@gmail.com>
2026-07-05 18:35:48 -0500

    Add LargestElement class to find and display the largest number in an array

---
+++ dsa/LargestElement.java
@@ -0,0 +1,15 @@
+class LargestElement
+{
+    public static void main(String s[])
+    {
+        int nums[] ={ 3, 2, 10, 11, -1, 29};
+        
+        int largest = nums[0];
+        for(int i=0; i < nums.length; i++)
+        {
+            if (nums[i] > largest)
+                largest = nums[i];
+        }
+        System.out.println("Largest element is: " + largest);
+    }
+}
