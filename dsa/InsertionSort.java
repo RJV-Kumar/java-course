ea9e013b218ac0ec5c1325539f24ce7bbb94b255
AutomateSphere <rajivkumar3636@gmail.com>
2026-07-15 23:05:49 -0500

    Add InsertionSort implementation with sorting demonstration

---
+++ dsa/InsertionSort.java
@@ -0,0 +1,29 @@
+class InsertionSort
+{
+    public static void main(String[] args) {
+        int[] nums = {3,5,6,4,8,9,10,7,1};
+        System.out.print("Insertion Sort, Before Sorting:  ");
+        for(int n : nums)
+        {
+            System.out.print(n + " ");
+        }
+
+        for(int i = 1; i < nums.length; i++)
+        {
+            int key = nums[i];
+            int j = i - 1;
+                
+            while(j >= 0 && nums[j] > key)
+            {
+                nums[j+1] = nums[j];
+                j--;
+            }
+            nums[j+1] = key;
+        }
+        System.out.print("\nAfter Sorting:  ");
+        for(int n : nums)
+        {
+            System.out.print(n + " ");
+        }
+    }
+}
