66485c28218b4b97c62600d64dda7ebe8a612d29
AutomateSphere <rajivkumar3636@gmail.com>
2026-07-23 22:12:52 -0500

    Add SecondLargest class with method to find the second largest element

---
+++ dsa/SecondLargest.java
@@ -0,0 +1,37 @@
+public class SecondLargest {
+
+    public int findSecondLargest(int[] nums)
+    {
+        int largest = 0;
+        int second = 0;
+
+        for(int i = 0; i < nums.length; i++)
+        {
+            if(nums[i] > largest)
+            {
+                second = largest;
+                largest = nums[i];
+            }
+            else if(nums[i] > second && nums[i] != largest)
+            {
+                second = nums[i];
+            }
+        }
+
+        return second;
+    }
+    public static void main(String[] args) {
+        int[] nums = {55, 32, 97, -55, 45, 32, 88, 21};
+
+        System.out.print("Input Array: ");
+        for(int n : nums)
+        {
+            System.out.print(n + " ");
+        }
+
+
+        SecondLargest obj = new SecondLargest();
+        System.out.println("\nSecond largest Element is: " + obj.findSecondLargest(nums));
+    }
+    
+}
